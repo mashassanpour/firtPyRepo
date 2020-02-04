501491e5248cea804137d681cdf0668de1e210d5
mashassanpour <59657592+mashassanpour@users.noreply.github.com>
2020-02-04 23:51:16 +0000

    Update Windows update registry stop.ps1

--- Windows update registry stop.ps1
+++ Windows update registry stop.ps1
@@ -6,9 +6,6 @@ function disable-winUpdateReg {
     $winPath = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows"
     if(Test-Path -path $winPath\WindowsUpdate){
         "Key already exist!"
-        # New-Item -path $winPath -Name "WindowsUpdate"
-        #New-Item -path $winPath\WindowsUpdate -Name "AU"
-        #New-ItemProperty -path $winPath\WindowsUpdate\AU -Name NoAutoUpdate -PropertyType DWord -Value 1
         return Get-ItemPropertyValue -path $winPath\WindowsUpdate\AU -Name NoAutoUpdate
     }
     else {
